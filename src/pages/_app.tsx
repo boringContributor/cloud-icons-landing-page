881fbe4667e33a33ef5c8e3c6ae98b64d8bb0ad6
Sebastian Sauerer <sebastian.sauerer@sap.com>
2022-05-21 13:22:02 +0200

    add splitbee

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import Head from "next/head";
+import Script from 'next/script';
 
 function Main({ Component, pageProps }: AppProps) {
   return (
@@ -8,6 +9,7 @@ function Main({ Component, pageProps }: AppProps) {
       <Head>
         <title>Cloud Icons</title>
         <link rel="shortcut icon" href="/cloud-icons.svg" />
+        <Script src="https://cdn.splitbee.io/sb.js" />
       </Head>
       <ChakraProvider>
         <Component {...pageProps} />
